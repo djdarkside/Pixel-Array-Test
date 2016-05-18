6a4a9226dba62eec83e3f8554d340c18fd4b836a
djdarkside <mitchellbaptist@gmail.com>
2016-05-18 00:19:18 -0700

    New

--- PixelArrayTest/src/com/djdarkside/game/keyboard/KeyBoard.java
+++ PixelArrayTest/src/com/djdarkside/game/keyboard/KeyBoard.java
@@ -1,5 +1,20 @@
 package com.djdarkside.game.keyboard;
 
-public class KeyBoard {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoard implements KeyListener {
+
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void keyPressed(KeyEvent e) {
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+		
+	}
 
 }
